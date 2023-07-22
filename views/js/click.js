776c2300b1a5dda8f49f0185bf4721eafb1e211d
Altan <admin@altan.me>
2023-07-22 21:14:24 +1200

    Fix Copy to Clipboard
    Added tooltip and refactored js for clipboard copy function

--- views/js/click.js
+++ views/js/click.js
@@ -1,17 +1,52 @@
+// console.log("JS LOADED");
+
+// var target = document.getElementById("click");
+
+// function copyTextToClipboard() {
+//   const element = document.getElementById("click");
+//   if (element && element.textContent.trim() !== "") {
+//     const textToCopy = element.textContent;
+//     navigator.clipboard
+//       .writeText(textToCopy)
+//       .then(() => {
+//         console.log("Text copied to clipboard:", textToCopy);
+//       })
+//       .catch((err) => {
+//         console.error("Error copying text to clipboard:", err);
+//       });
+//   } else {
+//     console.error(
+//       'Element with ID "click" does not exist or has no text content.'
+//     );
+//   }
+// }
+
+// target.onclick = function () {
+//   copyTextToClipboard();
+// };
+
 console.log("JS LOADED");
 
-var target = document.getElementById("click");
-console.log(target);
+document.addEventListener("click", function (event) {
+  const target = event.target;
+  if (target.id === "click") {
+    copyTextToClipboard(target.textContent);
+  }
+});
 
-function selection(elem) {
-  var elem = document.getElementById(elem);
-  var select = window.getSelection();
-  var range = document.createRange();
-
-  range.selectNodeContents(elem);
-  select.addRange(range);
+function copyTextToClipboard(text) {
+  if (text && text.trim() !== "") {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        console.log("Text copied to clipboard:", text);
+      })
+      .catch((err) => {
+        console.error("Error copying text to clipboard:", err);
+      });
+  } else {
+    console.error(
+      "Element with ID 'click' does not exist or has no text content."
+    );
+  }
 }
-
-target.onclick = function () {
-  selection("click");
-};
